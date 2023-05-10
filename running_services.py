6c3ff2f4c8a5786ac33e0ba8ab08c71bf2cf0aa8
John McCormack <john.mccormack@hostelworld.com>
2023-05-10 17:52:18 +0100

    working and some error handling

--- running_services.py
+++ running_services.py
@@ -14,27 +14,34 @@ client = logging_v2.Client(project=project_id)
 
 # Set the filter for the log query to only include logs from the past hour
 timestamp_format = "%Y-%m-%dT%H:%M:%S.%fZ"
-start_time = (datetime.utcnow() - timedelta(hours=1)).strftime(timestamp_format)
+start_time = (datetime.utcnow() - timedelta(hours=6)).strftime(timestamp_format)
 filter_str = f'logName="projects/{project_id}/logs/{log_name}" AND timestamp>="{start_time}"'
 
+try: 
+    # Execute the log query
+    response = client.list_entries(filter_=filter_str)
 
-# Execute the log query
-response = client.list_entries(filter_=filter_str)
+    # Extract the unique service names and their labels from the log entries
+    services = {}
+    for entry in response:
+        if "service_name" in entry.resource.labels:
+            service_name = entry.resource.labels["service_name"]
+            if service_name not in services:
+                services[service_name] = set()
+            if entry.labels is not None:
+                for key, value in entry.labels.items():
+                    if key.startswith("labels.") and key != "labels.resource_type":
+                        label_key = key[len("labels."):]
+                        services[service_name].add((label_key, value))
 
-# Extract the unique service names and their labels from the log entries
-services = {}
-for entry in response:
-    service_name = entry.resource.labels["service_name"]
-    if service_name not in services:
-        services[service_name] = set()
-    for key, value in entry.labels.items():
-        if key.startswith("labels.") and key != "labels.resource_type":
-            label_key = key[len("labels."):]
-            services[service_name].add((label_key, value))
+    # Print the list of running services and their labels
+    print("Running services:")
+    if len(services) == 0:
+        print("No running services found.")
+    for service_name, labels in services.items():
+        print(f"{service_name}:")
+        for label_key, label_value in labels:
+            print(f"\t{label_key}: {label_value}")
 
-# Print the list of running services and their labels
-print("Running services:")
-for service_name, labels in services.items():
-    print(f"{service_name}:")
-    for label_key, label_value in labels:
-        print(f"\t{label_key}: {label_value}")
+except GoogleAPIError as e:
+    print(f"Error executing log query: {e}")
